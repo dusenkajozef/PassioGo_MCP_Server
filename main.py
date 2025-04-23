846acaa14e42d6eb46c12ef2137f1d7260ab11e7
dusky_2 <jozef.dusenka.goalie@gmail.com>
2025-04-23 17:00:35 -0500

    added listing stops by transportation system

--- main.py
+++ main.py
@@ -21,8 +21,11 @@ def get_id_from_transportation_systems_map(transportation_system_name):
     
 
 def get_routes_from_transportation_system_id(transportation_system_id):
-
     return passiogo.getSystemFromID(transportation_system_id).getRoutes()
+
+
+def get_stops_from_transportation_system_id(transportation_system_id):
+    return passiogo.getSystemFromID(transportation_system_id).getStops()
 
 
 @mcp.tool()
@@ -53,13 +56,39 @@ def get_routes_from_transportation_system(transportation_system_name: str):
 
     return serializable_routes
 
+@mcp.tool()
+def get_stops_from_transportation_system(transportation_system_name: str):
+    """
+    Finds the informations about the stops of a transportation system by its name.
+
+    Args:
+        transportation_system_name: The approximate name of the transportation system (e.g., 'Georgia Tec', 'University of Arkansas', 'West Midtown Shuttle').
+
+    Returns:
+        A list of dictionaries containing the information about the stops.
+    """
+    transportation_system_id = get_id_from_transportation_systems_map(transportation_system_name)
+    if not transportation_system_id:
+        raise ValueError(f"Transportation system '{transportation_system_name}' not found.")
+    stops = get_stops_from_transportation_system_id(transportation_system_id)
+    if not stops:
+        raise ValueError(f"No stops found for transportation system ID '{transportation_system_id}'.")
+    
+    serializable_stops = []
+    for stop in stops:
+        stop_dict = vars(stop)
+        # Remove non-serializable attributes
+        if "system" in stop_dict:
+            del stop_dict["system"]        
+        serializable_stops.append(stop_dict)
+    return serializable_stops
 
 
 '''def main():
     print("Hello from passiogo-mcp-server!")
     print(f"Loaded {len(transportation_systems_map)} transportation systems.")
     approximate_name = input("Enter an approximate name: ")
-    routes = get_routes_from_transportation_system(approximate_name)
+    routes = get_stops_from_transportation_system(approximate_name)
     print(routes)'''
 
 
